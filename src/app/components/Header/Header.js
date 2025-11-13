ac16777e90fa912bd9134440503ef46c5cdc3ba1
Sohan Ac <sohanachhami655@gmail.com>
2025-11-13 17:36:14 +0545

    changes in header

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,16 +1,24 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import "@fontsource/arimo/400.css";
+import "@fontsource/arimo/700.css";
+
 
 const Header = () => {
   return (
     <>
-      <header>
+      <header className='flex justify-between'>
         <nav>
-             <div>
-                <Link to="/"> 
-                <Image>
-                     
-                </Image>
+             <div className='flex'>
+                <Link href="/" className='flex  justify-center items-center'> 
+                <div>
+                  <Image src="/logo.png" width={44} height={44}></Image>
+                </div>
+                
+                <div>
+                    <span className=''>NIET</span>
+                </div>
                  </Link>
              </div>
         </nav>
