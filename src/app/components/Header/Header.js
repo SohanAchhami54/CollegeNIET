836ab750d899060e4bc590d342eec2345e09152f
Sohan Ac <sohanachhami655@gmail.com>
2025-11-13 17:20:50 +0545

    image added

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <nav>
+             <div>
+                <Link to="/"> 
+                <Image>
+                     
+                </Image>
+                 </Link>
+             </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
