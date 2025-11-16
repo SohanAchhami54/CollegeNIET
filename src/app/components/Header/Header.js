b74f470d200b08c75769bddd9dc02568f4bd7caf
Sohan Ac <sohanachhami655@gmail.com>
2025-11-16 13:47:29 +0545

    header font changes

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -235,7 +235,7 @@ import SchoolIcon from "@mui/icons-material/School";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { getAllPrograms } from "@/data/programs";
 import { Scroll } from "lucide-react";
-
+import { graduateFont } from "@/font";
 const Header = () => {
   const [mobileView, setMobileView] = useState(false);
   const [scroll, setScroll] = useState(false);
@@ -289,21 +289,18 @@ useEffect(()=>{
       <nav className="max-w-[1400px] mx-auto px-6 lg:px-12">
         <div className="flex items-center justify-between h-20 lg:h-24">
           {/* Logo */}
-          <Link href="/" className="flex items-center gap-3 ">
+          <Link href="/" className="flex items-center gap-3">
           
             <Image
               src="/logo.png"
-              width={56}
-              height={56}
+              width={64}
+              height={64}
               alt="NIET_LOGO"
-             className="rounded-full transition-all duration-200    hover:scale-105 "
+             className={` rounded-full transition-all duration-200 hover:scale-105`}
 
             />
-
-         
-            
-            <div className="flex flex-col ">
-            <span className={`font-normal text-xl text-gray-900 ${scroll?'text-gray-900':'text-white'}`}>NIET</span>
+            <div className="flex flex-col gap-0 ">
+            <span className={`font-normal text-2xl text-gray-900 ${scroll?'text-gray-900':'text-white'} ${graduateFont.className}`}>NIET</span>
             <span className={`text-xs  opacity-55 ${scroll?'text-black':'text-white'}`}>(FORMER COLLEGE OF BIOMEDICAL ENGINEERING AND APPLIED SCIENCES)</span>
             </div>
             
