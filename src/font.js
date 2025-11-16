f5924565091ca253aba9cc45e57154d6097fd8ce
Sohan Ac <sohanachhami655@gmail.com>
2025-11-16 13:45:49 +0545

    font added

---
+++ src/font.js
@@ -0,0 +1,13 @@
+    import { Graduate, Roboto } from "next/font/google";
+
+    export const graduateFont = Graduate({
+    subsets: ["latin"],
+    weight: "400",
+    variable: "--font-graduate",
+    });
+
+    export const robotoFont = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto",
+    });
