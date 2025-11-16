607665b8571a38ea681c8e251a7e0db00cf5e09e
pixelace46-sudo <pixelace46@gmail.com>
2025-11-16 19:18:02 +0545

    programsection added

--- src/app/components/LandingPage/Programsection.js
+++ src/app/components/LandingPage/Programsection.js
@@ -7,7 +7,7 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { LuBrain } from "react-icons/lu";
 const Programsection = () => {
   return (
-    <section className='w-[1400px] mx-auto h-[1000px] relative z-10 top-32 mb-60 px-6 lg:px-12'>
+    <section className='w-100% mx-14 h-[1000px] relative z-10 top-32 mb-60 px-6 lg:px-12'>
       {/* first container  */}
         <div className='mb-16 lg:mb-12 '>
           {/* our program  */}
@@ -25,7 +25,52 @@ const Programsection = () => {
          <p className='text-xl text-gray-600 max-w-2xl'>Three cutting-edge programs designed to shape the fulture of technology and innovation.</p>
         </div>
 
-   {/* second container  */}
+  
+        <div className='grid md:grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8'>
+          {/* card */}
+         <div className='rounded-4xl overflow-hidden border border-black/10 hover:shadow-3xl  bg-white'>
+          {/* Image  */}
+            <div className='relative h-48 sm:h-56 md:h-64 overflow-hidden object-cover'>
+              <div>
+              <Image src='/BiomedicalLab.jpg' alt='BTechAI' width={700}  height={600} className='  ' />
+                </div>
+              {/* pink background color  */}
+             <div className='absolute inset-0 bg-gradient-to-tr from-pink-400/20 to-pink-300/20'></div>
+              {/* top badges  */}
+              <div className='absolute top-6 right-6 bg-white rounded-2xl px-3 py-3'>
+                <LuBrain className='w-8 h-7' />
+              </div>
+              <div className='absolute bottom-4 left-7 bg-white rounded-3xl py-2 px-3'>
+                <span> 4 Years • B. Tech</span>
+              </div>
+            </div>
+            {/* content section  */}
+            <div className='p-8'>
+              <h2 className={`text-2xl ${graduateFont.className} mb-4 `} >
+                B. Tech in artificial Intelligence
+                {/* <br />
+                <span>Intelligence</span> */}
+              </h2>
+              <span className='bg-green-600 px-2 rounded-sm text-white 
+              '>NEW</span>
+              
+              <p className='text-lg  tracking-normal mb-5'>The Bachelor of Technology in Artificial Intelligence integrates computer science, mathematics, and data science to develop problem- solving skills and build intelligent systems for real-world applications</p>
+               
+               <button className='text-lg font-semibold  text-blue-600 flex gap-7 justify-center items-center'>
+                <span>Explore Programs</span>
+                 <ArrowForwardIcon/>
+               </button>
+            </div>
+           
+         </div>
+ 
+</div>
+    </section>
+  )
+}
+
+export default Programsection
+ {/* second container  */}
         {/* <div className='grid lg:grid-cols-3 gap-6 lg:gap-8 '>
 
 
@@ -53,26 +98,3 @@ const Programsection = () => {
               
           </div>
         </div> */}
-        <div className='grid lg:grid-cols-3 gap-6 lg:gap-8'>
-          {/* card */}
-         <div className='rounded-4xl overflow-hidden shadow-2xl hover:shadow-3xl  bg-red-300'>
-          {/* Image  */}
-            <div className='relative h-64 overflow-hidden'>
-              <Image src='/BiomedicalLab.jpg' alt='BTechAI' width={600} height={400} className='w-full h-full object-cover ' />
-
-              {/* pink background color  */}
-              <div className='absolute inset-0 bg-gradient-to-tr from-pink-400/20 to-pink-300/20'></div>
-              {/* top badges  */}
-              <div className='absolute top-6 right-6 bg-white rounded-2xl px-3 py-3'>
-                <LuBrain className='w-8 h-7' />
-              </div>
-            </div>
-           
-         </div>
- 
-</div>
-    </section>
-  )
-}
-
-export default Programsection
