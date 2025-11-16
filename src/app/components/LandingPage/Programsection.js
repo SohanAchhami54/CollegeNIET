e21f1633eeb11db300bdf6465067921ef58b6be8
Sohan Ac <sohanachhami655@gmail.com>
2025-11-16 11:44:48 +0545

    axios added to make request to backend

---
+++ src/app/components/LandingPage/Programsection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Programsection = () => {
+  return (
+    <div>
+         <div>
+            <div></div>
+            <div></div>
+         </div>
+         <div></div>
+    </div>
+  )
+}
+
+export default Programsection
