b012ab90ad338fa1ade109c89b3f2bad1ae4e894
pixelace46-sudo <pixelace46@gmail.com>
2025-11-19 21:46:56 +0545

    fixed the reusable building components

--- src/app/components/ReusableComHero/Reusablebuilding.js
+++ src/app/components/ReusableComHero/Reusablebuilding.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Reusablebuilding = () => {
   return (
     <div>
-        <div className='absolute inset-0 z-1'>
+        <div className='absolute inset-0 z-0'>
                   <Image src="/building.jpg" alt="NIET Campus" className='w-full h-full object-cover opacity-20' 
                   width={1000} height={900}></Image>
                 </div>
