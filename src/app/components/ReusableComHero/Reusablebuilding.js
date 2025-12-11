8587bf61a0300449327cc73d95dd3da6ad3e7d4e
pixelace46-sudo <pixelace46@gmail.com>
2025-12-11 12:07:47 +0545

    reusable building added

--- src/app/components/ReusableComHero/Reusablebuilding.js
+++ src/app/components/ReusableComHero/Reusablebuilding.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Reusablebuilding = () => {
+const Reusablebuilding = ({ path }) => {
   return (
     <div>
-        <div className='absolute inset-0 z-1'>
-                  <Image src="/building.jpg" alt="NIET Campus" className='w-full h-full object-cover opacity-20 ' 
-                  width={1000} height={900}></Image>
-                </div>
+      <div className='absolute inset-0 z-1'>
+        {console.log('path', path)}
+        <Image src={`https://biomedical.edu.np${path}`} alt="NIET Campus" className='w-full h-full object-cover opacity-20 '
+          width={1000} height={900}></Image>
+      </div>
     </div>
   )
 }
